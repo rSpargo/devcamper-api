df28ca04d70d64a1b382522a84a0880e558942ee
rSpargo <ryanspargo@gmail.com>
2023-01-18 13:28:02 -0600

    Initial commit / publish course results to git

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+
+// protect routes
+exports.protect = asyncHandler(async (req, res, next)  => {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  // make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Not authorized to access this route', 401));
+  }
+
+  try {
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    return next(new ErrorResponse('Not authorized to access this route', 401));
+  }
+});
+
+// grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(`User role ${req.user.role} is unauthorized to access this route`, 403));
+    }
+    next();
+  }
+}
---
+++ server.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+// middleware files
+const errorHandler = require('./middleware/error');
+// const logger = require('./middleware/logger');
+
+// load env variables
+dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+
+
+const app = express();
+
+// body parser
+app.use(express.json());
+
+// cookie parser
+app.use(cookieParser());
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// file uploading
+app.use(fileUpload());
+
+// sanitize data
+app.use(mongoSanitize);
+
+// set security headers
+app.use(helmet);
+
+// prevent XSS attacks
+app.use(xss);
+
+// rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100
+});
+
+app.use(limiter);
+
+// prevent http param pollution
+app.use(hpp());
+
+// enable CORS
+app.use(cors);
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server and exit process
+  server.close(() => {
+    process.exit(1);
+  })
+})
